14474a8d63728387f7996ea3e3365358253e22b9
PrincessTina <kristina.orl-va@bk.ru>
2018-12-15 19:02:49 +0300

    add ability to display time and date

--- react_slider/src/js/components/Slider/Slider.jsx
+++ react_slider/src/js/components/Slider/Slider.jsx
@@ -11,109 +11,114 @@ export default class Slider extends React.Component {
         super(props);
 
         this.state = {
-          leftRangeValue: this.props.leftRangeValue,
-          rightRangeValue: this.props.rightRangeValue
+            leftRangeValue: this.props.leftRangeValue,
+            rightRangeValue: this.props.rightRangeValue
         };
 
-        this.onRightRangeChange = this.onRightRangeChange.bind(this);
-        this.onLeftRangeChange = this.onLeftRangeChange.bind(this);
-        this.getBackgroundStyle = this.getBackgroundStyle.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     /**
-     * Ставит новый кейс
+     * Вызывается при изменении значений инпутов
      *
-     * @param id - номер кейса
-     *
-     * @props onChange
+     * @param element
      */
-    changeCase(id) {
-        if (id !== this.props.case) {
-            this.props.onChange(id);
+    onChange(element) {
+        if (element.target.classList.contains('slider__range--left')) {
+            this.setState({
+                leftRangeValue: element.target.value
+            });
+
+            this.props.onChange(element.target.value, this.state.rightRangeValue);
+        } else if (element.target.classList.contains('slider__range--right')) {
+            this.setState({
+                rightRangeValue: element.target.value
+            });
+
+            this.props.onChange(this.state.leftRangeValue, element.target.value);
         }
     }
 
     /**
-     * Отдает класс строки в меню по ее id
+     * Отдает background стиль для инпута (левого или правого)
      *
-     * @param id - номер строки
-     *
+     * @param min
+     * @param max
+     * @param location - left/right
      * @return {string}
      */
-    getRowClass(id) {
-        return (id === this.props.case) ? 'tabsMenu__row--selected' : 'tabsMenu__row';
-    }
-
-    /**
-     * Отдает расположение слайдера, исходя из номера текущего кейса
-     *
-     * @return {Object}
-     */
-    getSliderStyle() {
-        return {
-            top: 21 + 32 * this.props.case
-        };
-    }
-
-    /**
-     * Отдает верстку для всех кейсов
-     *
-     * @props array
-     *
-     * @return {Array}
-     */
-    createMenuCases() {
-        const array = this.props.array;
-        let content = [];
-
-        for (let i = 0; i < array.length; i++) {
-            content.push(
-                <div className={this.getRowClass(i)} key={i} onClick={() => this.changeCase(i)}>{array[i]}</div>
-            );
-        }
-
-        return content;
-    }
-
-    onRightRangeChange(element) {
-        this.setState({
-            rightRangeValue: element.target.value
-        });
-    }
-
-    onLeftRangeChange(element) {
-        this.setState({
-            leftRangeValue: element.target.value
-        });
-    }
-
     getBackgroundStyle(min, max, location) {
         if (location === 'left') {
             let result = 100 - Math.abs(this.state.leftRangeValue) * 100 / (max - min);
 
-            return "repeating-linear-gradient(to right, #d7dcdf 0%, #d7dcdf " + result +
-                "%, rgba(180, 0, 0, 0.8) 0, rgba(180, 0, 0, 0.8) 100%)";
+            return 'repeating-linear-gradient(to right, #d7dcdf 0%, #d7dcdf ' + result +
+                '%, rgba(180, 0, 0, 0.8) 0, rgba(180, 0, 0, 0.8) 100%)';
         } else if (location === 'right') {
             let result = 100 - Math.abs(this.state.rightRangeValue) * 100 / (max - min);
 
-            return "repeating-linear-gradient(to left, #d7dcdf 0%, #d7dcdf " + result +
-                "%, rgba(0, 80, 0, 0.8) 0, rgba(0, 80, 0, 0.8) 100%)";
+            return 'repeating-linear-gradient(to left, #d7dcdf 0%, #d7dcdf ' + result +
+                '%, rgba(0, 80, 0, 0.8) 0, rgba(0, 80, 0, 0.8) 100%)';
+        }
+    }
+
+    /**
+     * Формирует время в нормальном формате по значению даты в миллисекундах
+     *
+     * @param date
+     * @return {string}
+     */
+    timeStr(date) {
+        return (date.getTime() === 86400000) ? '00:00' :
+            (date.getUTCHours() < 10 ? "0" : "") + date.getUTCHours() + ":" + (date.getUTCMinutes() < 10 ? "0" : "") + date.getUTCMinutes();
+    }
+
+    /**
+     * Формирует дату в нормальном формате по ее значению в миллисекундах
+     *
+     * @param date
+     * @return {string}
+     */
+    dateStr(date) {
+        return (date.getDate() < 10 ? "0" : "") + date.getDate() + "-" + (date.getMonth() + 1 < 10 ? "0" : "") +
+            (date.getMonth() + 1) + "-" + (date.getFullYear());
+    }
+
+    /**
+     * Преобразует выбранное значение к формату даты/времени/процентов
+     *
+     * @param value
+     * @param sliderClass
+     * @return {string}
+     */
+    transformValue(value, sliderClass) {
+        if (sliderClass.indexOf('time') !== -1) {
+            if (sliderClass.indexOf('hours') !== -1) {
+                return this.timeStr(new Date(Math.abs(value)));
+            } else {
+                return this.dateStr(new Date(Math.abs(value)));
+            }
+        } else {
+            return value + '%';
         }
     }
 
     render() {
+        let sliderClass = 'slider time';
+
         return (
-            <div className="slider">
-                <div className={"slider__block"}>
-                    <input type="range" className="slider__range--left" value={this.state.leftRangeValue} min="-100" max="0"
-                    style={{background: this.getBackgroundStyle(-100, 0, 'left')}} onChange={this.onLeftRangeChange}/>
-                    <div className="slider__value--left">{this.state.leftRangeValue}</div>
+            <div className={sliderClass}>
+                <div className={'slider__block'}>
+                    <input type='range' className='slider__range--left' value={this.state.leftRangeValue} min='-100'
+                           max='0'
+                           style={{background: this.getBackgroundStyle(-100, 0, 'left')}} onChange={this.onChange}/>
+                    <div className='slider__value--left'>{this.transformValue(this.state.leftRangeValue, sliderClass)}</div>
                 </div>
-                <div className="slider__middleBlock"/>
-                <div className={"slider__block"}>
-                    <input type="range" className="slider__range--right" value={this.state.rightRangeValue} min="0" max="1000"
-                           style={{background: this.getBackgroundStyle(0, 1000, 'right')}} onChange={this.onRightRangeChange}/>
-                    <div className="slider__value--right">{this.state.rightRangeValue}</div>
+                <div className='slider__middleBlock'/>
+                <div className={'slider__block'}>
+                    <input type='range' className='slider__range--right' value={this.state.rightRangeValue} min='0'
+                           max='1000'
+                           style={{background: this.getBackgroundStyle(0, 1000, 'right')}} onChange={this.onChange}/>
+                    <div className='slider__value--right'>{this.transformValue(this.state.rightRangeValue, sliderClass)}</div>
                 </div>
             </div>
         );
@@ -124,7 +129,7 @@ Slider.propTypes = {
     /**
      * Значение левого инпута
      */
-    leftRangeValue: PropTypes.number.isRequired,
+    leftRangeValue: PropTypes.number,
 
     /**
      * Значение правого инпута
@@ -140,9 +145,8 @@ Slider.propTypes = {
 Slider.defaultProps = {
     leftRangeValue: -75,
     rightRangeValue: 75,
-    onChange: function () {
-        console.log('leftRange: ' + this.state.leftRangeValue);
-        console.log('rightRange: ' + this.state.rightRangeValue);
-        console.log();
+    onChange: function (leftRangeValue, rightRangeValue) {
+        console.log('leftRange: ' + leftRangeValue);
+        console.log('rightRange: ' + rightRangeValue + '\n');
     },
 };
